1f532d8355a49031d88f6bdb943481a833f56334
Mxolisi Mthanti <mxolisimthanti1910@gmail.com>
2025-08-20 15:30:24 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for contacting Mthanti Tech Solutions! We'll get back to you soon.");
+});
